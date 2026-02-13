31f44037b478aa82cb65dace0f85f0ed48b99cde
PhilippNaused <philipp.naused@t-online.de>
2026-02-13 19:57:37 +0100

    Improve Randomizer

--- Tests/Dummies/ICSharp.Tests/Snapshots/StringsTest/Decompile.cs
+++ Tests/Dummies/ICSharp.Tests/Snapshots/StringsTest/Decompile.cs
@@ -1,14 +1,14 @@
 public class Strings
 {
-    public static string Field1 = a.b[6];
-    public string Field2 = a.b[4];
-    public static string Prop1 { get; } = a.b[7];
+    public static string Field1 = a.b[1];
+    public string Field2 = a.b[2];
+    public static string Prop1 { get; } = a.b[4];
     public string Prop2 { get; } = a.b[5];
-    public static string Prop3 => a.b[0];
-    public string Prop4 => a.b[1];
+    public static string Prop3 => a.b[6];
+    public string Prop4 => a.b[7];
     public string Invoke1()
     {
-        return a.b[2];
+        return a.b[0];
     }
     public string Invokeÿǿ()
     {
